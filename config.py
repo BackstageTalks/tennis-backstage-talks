9340841baaad90145c39e2eeceb2aebf321e9097
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-06-27 17:31:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
-# config.py
+API_BASE_URL = "https://sports.bzzoiro.com/tennis/api/v2"
 
-API_BASE_URL = "https://api.bsdapi.com/tennis/api/v2"
-
-TARGET_CATEGORIES = [
-    "ATP",
-    "WTA",
-    "Challenger"
-]
+TARGET_CATEGORIES = ["ATP", "WTA", "Challenger"]
 
 MAX_MATCHES = None
