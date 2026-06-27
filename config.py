f8978b982ea53a2c1efda8e33c38a853f519da6f
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-06-27 23:01:08 +0200

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-API_BASE_URL = "https://sports.bzzoiro.com/tennis/api/v2"
-
-# Sem vlož svoj API token
-API_TOKEN = "9d565257..."
-
-TARGET_CATEGORIES = ["ATP", "WTA", "Challenger"]
-
-MAX_MATCHES = None
