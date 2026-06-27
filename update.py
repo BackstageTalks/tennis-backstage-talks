de0c3a3885881e13b744f57c3144b069ed0c48d3
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-06-27 14:02:34 +0200

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,21 @@
+from welo import WElo
+from api import get_recent_matches
 
+w = WElo()
+
+matches = get_recent_matches(tour="ATP", limit=200)
+
+for m in matches:
+    winner = m["winner"]
+    loser = m["loser"]
+
+    gw_winner = m["winner_games"]
+    gw_loser = m["loser_games"]
+
+    surface = m["surface"]
+
+    w.update(winner, loser, gw_winner, gw_loser, surface)
+
+# Test predikcie
+print("Nadal vs Djokovic (Grass):", w.predict("Nadal", "Djokovic", "Grass"))
+print("Ruud vs Alcaraz (Clay):", w.predict("Ruud", "Alcaraz", "Clay"))
