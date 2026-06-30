73f88c0bf2e17fd9666d9b13a165dbfd2a2d7c98
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-06-30 14:05:31 +0200

    Delete form_fetcher.py

--- form_fetcher.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def get_player_form(player):
-    # stabilná forma (rovnaký hráč = rovnaký výsledok)
-    random.seed(hash(player) % 100000)
-    wins = random.randint(4, 8)
-    return wins / 10
