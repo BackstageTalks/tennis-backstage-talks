9b0166516fe1a6bff01a7bd7d4ce8811e80a77d6
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-07-01 06:46:33 +0200

    Implement prediction functions using build_all_predictions

---
+++ prediction_engine_all.py
@@ -0,0 +1,13 @@
+from prediction_engine import build_all_predictions
+
+
+def get_all_predictions():
+    return build_all_predictions()
+
+
+def build_predictions_all():
+    return build_all_predictions()
+
+
+def get_daily_predictions_all():
+    return build_all_predictions()
