4437e9f8cc5ac915c6221265b63587ea86d4ba90
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-07-04 13:21:28 +0200

    Update __init__.py

--- src/marq_ai/__init__.py
+++ src/marq_ai/__init__.py
@@ -1,7 +1,9 @@
 from .service import build_marq_ai
 from .provider import fetch_marq_market_data
+from .pipeline import build_marq_from_match
 
 __all__ = [
     "build_marq_ai",
     "fetch_marq_market_data",
+    "build_marq_from_match",
 ]
