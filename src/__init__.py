e0907199227b7a8c58577cf523b7f52837bf2107
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-07-04 15:24:34 +0200

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Root package marker for project modules.
+
+This file must stay lightweight. Do not import submodules here, because
+project code imports packages such as src.marq_ai and src.models directly.
+"""
+
+__all__ = []
