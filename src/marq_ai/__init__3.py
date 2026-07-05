495fc0ee9a80f7a91357c2a6f480fd6d5d4978fb
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-07-05 08:53:20 +0200

    Delete src/marq_ai/__init__3.py

--- src/marq_ai/__init__3.py
+++
@@ -1,9 +0,0 @@
-from .service import build_marq_ai
-from .provider import fetch_marq_market_data
-from .pipeline import build_marq_from_match
-
-__all__ = [
-    "build_marq_ai",
-    "fetch_marq_market_data",
-    "build_marq_from_match",
-]
